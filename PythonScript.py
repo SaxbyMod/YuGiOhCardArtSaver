ddc4a5a538c2d9cec4b922455d2e4bcc79bd08ac
Geck Man <102002463+SaxbyMod@users.noreply.github.com>
2023-07-30 19:57:09 -0400

    Add files via upload

--- PythonScript.py
+++ PythonScript.py
@@ -21,28 +21,44 @@ def fetch_card_data(card_name):
 def resize_image(image, size):
     return image.resize(size, Image.BILINEAR)
 
-# Card name to search for, taken as input from the user
-card_name = input("Enter the name of the card: ")
+while True:
+    # Card name to search for, taken as input from the user
+    card_name = input("Enter the name of the card (or 'quit' to stop): ")
+    
+    if card_name.lower() == 'quit':
+        break
 
-# Fetch card data from the API
-card_data = fetch_card_data(card_name)
+    # Fetch card data from the API
+    card_data = fetch_card_data(card_name)
 
-if card_data:
-    # Get the card image URL
-    image_url = card_data[0]['card_images'][0]['image_url']
-    response = requests.get(image_url)
+    if card_data:
+        # Get the card image URL
+        image_url = card_data[0]['card_images'][0]['image_url']
+        response = requests.get(image_url)
 
-    # Open the image and crop it to the desired region (49,111) - (371,433)
-    img = Image.open(BytesIO(response.content))
-    img = img.crop((49, 111, 371, 433))
+        # Open the image and crop it to the desired region (49,111) - (371,433)
+        img = Image.open(BytesIO(response.content))
+        img = img.crop((49, 111, 371, 433))
 
-    # Save the cropped image
-    img.save(f'Output/{card_name}_art.jpg')
-    print(f"The card art of '{card_name}' has been saved in the Output folder.")
+        # Save the cropped image
+        img.save(f'Output/{card_name}_art.jpg')
+        print(f"The card art of '{card_name}' has been saved in the Output folder.")
 
-    # Shrink down the cropped image to 114x94 using Bilinear scaling
-    resized_img = resize_image(img, (114, 94))
-    resized_img.save(f'Output/{card_name}_shrunken.jpg')
-    print(f"The shrunken image of '{card_name}' has been saved in the Output folder.")
-else:
-    print(f"No card found for '{card_name}'.")
+        # Shrink down the cropped image to 114x94 using Bilinear scaling
+        resized_img = resize_image(img, (114, 94))
+        resized_img.save(f'Output/{card_name}_shrunken.jpg')
+        print(f"The shrunken image of '{card_name}' has been saved in the Output folder.")
+
+        # Print card details
+        card = card_data[0]
+        print(f"Name: {card.get('name')}")
+        print(f"Type: {card.get('type')}")
+        print(f"Attribute: {card.get('attribute')}")
+        print(f"Race (Typing): {card.get('race')}")
+        print(f"Level/Rank: {card.get('level')}")
+        print(f"ATK: {card.get('atk')}")
+        print(f"DEF: {card.get('def')}")
+        print(f"Archetype: {card.get('archetype')}")
+        print(f"Card Text: {card.get('desc')}")
+    else:
+        print(f"No card found for '{card_name}'.")
